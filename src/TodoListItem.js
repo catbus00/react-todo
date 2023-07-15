f1570162b49bfa437536c2b166ab689c9cd888af
catbus00 <catmousekill@gmail.com>
2023-07-15 13:27:26 +0700

    Add an onClick prop to the button element and pass a function that calls onRemoveTodo from props with the current item id as an argument

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-function TodoListItem({todo, onClick}) {
+function TodoListItem({todo, removeTodo}) {
+    const onRemoveClick = () => {
+        removeTodo(todo.id);
+      };
+
     return(
         <li>
             {todo.title}
-            <button onClick={onClick}>Remove</button>
+            <button onClick={onRemoveClick}>Remove</button>
         </li>
 
     );
