f33438f9747d42460a2a6861d0c75d2e9b754439
catbus00 <catmousekill@gmail.com>
2023-07-24 15:12:53 +0700

    Delete useSemiPersistentState function call from App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import React, {useState, useEffect} from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem('savedTodoList')) || []
-  );
-
   useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList])
