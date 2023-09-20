353348c6a69a34046b869ed2333f9712e2c00965
catbus00 <catmousekill@gmail.com>
2023-09-20 13:10:46 +0700

    Update dependencies and lint code and refactor methods in API

--- src/App.js
+++ src/App.js
@@ -1,137 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
+import style from './components/App.module.css';
+import './components/App.css';
+import api from './components/Api';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [checkedState, setCheckedState] = useState({});
 
   const fetchAndSortTodos = async () => {
-    const options = {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`
-      }
-    };
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      const message = `Error: ${response.status}`;
-      throw new Error(message);
-    }
-    const data = await response.json();
-    const todos = data.records.map((todo) => ({
-      title: todo.fields.title,
-      id: todo.id,
-      createdTime: todo.createdTime,
-    }))
+    const sortedTodos = await api.fetchAndSortTodos();
 
-    const sortedTodos = todos.sort((a, b) => {
-        if (a.createdTime > b.createdTime) {
-            return 1;
-        } else if (a.createdTime < b.createdTime) {
-            return -1;
-        } else {
-            return 0;
-        }
-    });
-
-      setTodoList(sortedTodos);
-      setIsLoading(false);
-    };
-
-    useEffect(() => {
-      fetchAndSortTodos();
-    }, []);
-    
-  const postTodo = async (todo) => {
-    const newTodo = {
-      fields: {
-        title: todo.title,
-      },
-    };
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-      },
-      body: JSON.stringify(newTodo),
-    };
-  
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`;
-  
-    try{
-      const response = await fetch(url, options);
-      if (!response.ok) {
-        const message = `Error has occurred: ${response.status}`;
-        throw new Error(message);
-      }
-
-      const dataResponse = await response.json();
-      return dataResponse;
-
-    } catch (error) {
-      console.log(error.message)
-      return null;
-    }
-};
-
- const removeTodo = async (id) => { 
-  const deleteUrl = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default/${id}`;
-  const deleteOptions = {
-    method: 'DELETE',
-    headers: {
-      Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-    },
+    setTodoList(sortedTodos);
+    setIsLoading(false);
   };
-  try {
-  const response = await fetch(deleteUrl, deleteOptions)
-    if (!response.ok) {
-      throw new Error(`Error deleting todo: ${response.status}`);
-    } 
-    setTodoList(prevTodoList => prevTodoList.filter(todo => todo.id !== id));
-    } catch (error) {
-      console.log('Delete Error:', error.message);
-    };
+
+  useEffect(() => {
+    fetchAndSortTodos();
+  }, []);
+
+  const removeTodo = async (id) => {
+    const response = await api.removeTodo(id);
+    if (response === id) {
+      setTodoList((prevTodoList) => prevTodoList.filter((todo) => todo.id !== id));
+    }
   };
 
   const addTodo = async (newTodo) => {
-    const dataResponse = await postTodo(newTodo);
-    if (dataResponse) {
-      newTodo.id = dataResponse.id;
-      setTodoList([...todoList, newTodo])
-      }
+    const id = await api.addTodo(newTodo);
+    if (id !== undefined) {
+      const todo = newTodo;
+      todo.id = id;
+      setTodoList([...todoList, todo]);
     }
+  };
+
+  const handleChange = (id) => {
+    setCheckedState((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  };
+
+  // http://my.domain.com // landing page // App.js
+  // http://my.domain.com/todos // displaying all the todos // TodoList.js (show all state)
+
+  // http://my.domain.com/todo // specific todo (any state for specific todo)
+  // http://my.domain.com/todo/1234 // any operation with specific todo e.g. with ID 1234 (default to show specific todo)
+  // or
+  // http://my.domain.com/todo/new // creating new todo (display specific one state)
+  // http://my.domain.com/todo/show/1234 // displaying todo with ID 1234 (display specific one state)
+  // http://my.domain.com/todo/edit/1234 // editing todo with ID 1234 (display specific one state)
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/new" element = {<h1>New Todo List</h1>} />
-        <Route 
-          path="/" 
-          element = {
-          <>
-            <h1>Todo List</h1>
+        <Route path="/todo" element={<h1>New Todo List</h1>} />
+        <Route path="/todo" element={<h1>New Todo List</h1>} />
+        <Route
+          path="/"
+          element={(
+            <div className={style.ListItem}>
+              <h1><b>TASK LIST</b></h1>
               {isLoading ? (
-              <>
                 <p>Loading...</p>
-              </>
-            ) : (
-              <>
-
-                  <AddTodoForm 
-                    onAddTodo={addTodo} 
+              ) : (
+                <>
+                  <TodoList
+                    todoList={todoList}
+                    removeTodo={removeTodo}
+                    isChecked={checkedState}
+                    handleChange={handleChange}
                   />
-                  <TodoList 
-                    todoList={todoList} 
-                    removeTodo={removeTodo} 
+                  <AddTodoForm
+                    onAddTodo={addTodo}
                   />
-                  </>
-                )}
-              </>
-            }
+                </>
+              )}
+            </div>
+        )}
         />
       </Routes>
     </BrowserRouter>
